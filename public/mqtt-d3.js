fbad3b057929d8b997d9fac09f51d278de686882
Ylihärsilä <Mikko.Yliharsila@seamk.fi>
2020-03-14 09:40:49 +0200

    commit

--- public/mqtt-d3.js
+++ public/mqtt-d3.js
@@ -26,14 +26,98 @@ let j1 = d3.line()
     .y((d) => {
         return y_scale(d.joints[0]);
     });
+
+let j2 = d3.line()
+    .x((d) => {
+        return x_scale(Date.parse(d.time));
+    })
+    .y((d) => {
+        return y_scale(d.joints[1]);
+    });
+
+let j3 = d3.line()
+    .x((d) => {
+        return x_scale(Date.parse(d.time));
+    })
+    .y((d) => {
+        return y_scale(d.joints[2]);
+    });
+
+let j4 = d3.line()
+    .x((d) => {
+        return x_scale(Date.parse(d.time));
+    })
+    .y((d) => {
+        return y_scale(d.joints[3]);
+    });
+
+let j5 = d3.line()
+    .x((d) => {
+        return x_scale(Date.parse(d.time));
+    })
+    .y((d) => {
+        return y_scale(d.joints[4]);
+    });
+
+let j6 = d3.line()
+    .x((d) => {
+        return x_scale(Date.parse(d.time));
+    })
+    .y((d) => {
+        return y_scale(d.joints[5]);
+    });
+
 g.append("g").attr("class", "axis x");
 g.append("g").attr("class", "axis y");
+
+//J1
 g.append("path")
     .datum(joint_datas)
     .attr("class", "j1")
     .attr("d", j1)
     .style('fill', 'none')
     .style('stroke', '#00F');
+
+//J2
+g.append("path")
+    .datum(joint_datas)
+    .attr("class", "j2")
+    .attr("d", j2)
+    .style('fill', 'none')
+    .style('stroke', '#0F0');
+
+//J3
+g.append("path")
+    .datum(joint_datas)
+    .attr("class", "j3")
+    .attr("d", j3)
+    .style('fill', 'none')
+    .style('stroke', '#F00');
+
+//J4
+g.append("path")
+    .datum(joint_datas)
+    .attr("class", "j4")
+    .attr("d", j4)
+    .style('fill', 'none')
+    .style('stroke', '#0FF');
+
+//J5
+g.append("path")
+    .datum(joint_datas)
+    .attr("class", "j5")
+    .attr("d", j5)
+    .style('fill', 'none')
+    .style('stroke', '#FF0');
+
+//J6
+g.append("path")
+    .datum(joint_datas)
+    .attr("class", "j6")
+    .attr("d", j6)
+    .style('fill', 'none')
+    .style('stroke', '#AAA');
+
 const resize = () => {
     width = window.innerWidth - margin.left - margin.right
     height = window.innerHeight - margin.top - margin.bottom;
@@ -60,6 +144,21 @@ const update_chart = () => {
         .call(d3.axisLeft(y_scale));
     svg.select(".j1") // change the line
         .attr("d", j1(joint_datas));
+
+    svg.select(".j2") // change the line
+        .attr("d", j2(joint_datas));
+
+    svg.select(".j3") // change the line
+        .attr("d", j3(joint_datas));
+
+    svg.select(".j4") // change the line
+        .attr("d", j4(joint_datas));
+
+    svg.select(".j5") // change the line
+        .attr("d", j5(joint_datas));
+
+    svg.select(".j6") // change the line
+        .attr("d", j6(joint_datas));
 };
 
 
